e6c06a832a37dae529306bd0af997b2a8e2f77ab
MyT25 <marco.antonio.noh1999@gmail.com>
2019-12-05 18:25:44 -0600

    ARCHIVO PARA LEER

---
+++ ARCHIVOLEXICO.cpp
@@ -0,0 +1,27 @@
+int valor1 = 3;
+int valor2 = 56;
+int count;
+int suma = 45 + 3.6;
+float resta = 67 - 2.57;
+int multiplicacion = 43 * 554;
+int division = 10 / 2;
+int a;
+double df = 54;
+ 
+a = 12 * ( 2 + 5.2)
+bool b = true;
+
+
+if (a == b)
+{
+	
+}else{
+
+}
+
+for (int i = 0; i < count; ++i)
+{
+	
+}
+
+while(1<10)
